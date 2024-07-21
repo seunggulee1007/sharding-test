c402957d1a926412d652c65b042b09e972c65061
이승구 <leesg107@naver.com>
2024-07-21 22:00:30 +0900

    feat: sharding test

--- src/main/java/com/yeseung/sharding/ShardingApplication.java
+++ src/main/java/com/yeseung/sharding/ShardingApplication.java
@@ -3,7 +3,7 @@ package com.yeseung.sharding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.yeseung.sharding", "org.apache.shardingsphere.shardingjdbc.spring.boot"})
 public class ShardingApplication {
 
     public static void main(String[] args) {
